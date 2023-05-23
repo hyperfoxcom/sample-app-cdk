05e75ef5aeebddf3e36a90e976e8b99b2c2bd658
Eduard <eduard.cespedes@hyperfox.com>
2023-05-23 10:31:07 +0200

    Adding example on EC2 and load balancers

--- bin/sample-app-cdk.ts
+++ bin/sample-app-cdk.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { SampleAppCdkStack } from '../lib/sample-app-cdk-stack';
+import { FoxyCdkStack } from '../lib/foxy_cdk-stack';
 
 const app = new cdk.App();
 new SampleAppCdkStack(app, 'SampleAppCdkStack');
+
+new FoxyCdkStack(app, 'FoxyCdkStack');
